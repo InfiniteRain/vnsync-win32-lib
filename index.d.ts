3f6415fd44aba42dc97019f786f0ddf041b21baa
David Lysenko <lysenkodavid@gmail.com>
2021-04-08 23:06:00 +0300

    Add index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,23 @@
+export interface Point {
+  x: number;
+  y: number;
+}
+
+export interface Rectangle {
+  left: number;
+  top: number;
+  right: number;
+  buttom: number;
+}
+
+export interface Window {
+  handle: number;
+  title: string;
+}
+
+export function getOpenedWindows(): Window[];
+export function showWindow(handle: number): void;
+export function getWindowRectangle(handle: number): Rectangle;
+export function getCursorPosition(): Point;
+export function setCursorPosition(x: number, y: number): void;
+export function mouseClick(): void;
