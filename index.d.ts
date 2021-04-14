b2a6f5ea325a599e25a21987ae55d5d877e433f7
David Lysenko <lysenkodavid@gmail.com>
2021-04-14 18:13:16 +0300

    Fix type annotation for windowExists

--- index.d.ts
+++ index.d.ts
@@ -16,7 +16,7 @@ export interface Window {
 }
 
 export function getOpenedWindows(): Window[];
-export function windowExists(handle: number): void;
+export function windowExists(handle: number): boolean;
 export function showWindow(handle: number): void;
 export function getWindowRectangle(handle: number): Rectangle;
 export function getCursorPosition(): Point;
